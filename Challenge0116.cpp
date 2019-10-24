5050e407b8c422d2370f6fe45441a0eb7144eaca
Adrián Navarro Gabino <adnaga94@gmail.com>
2019-10-24 10:57:21 +0200

    Challenge 116 - ¡Hola mundo!

---
+++ Challenge0116.cpp
@@ -0,0 +1,17 @@
+// Adrián Navarro Gabino
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int times;
+    cin >> times;
+    
+    for(int i = 0; i < times; i++)
+    {
+        cout << "Hola mundo." << endl;
+    }
+	return 0;
+}
