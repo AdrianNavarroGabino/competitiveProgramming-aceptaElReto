f0601326437fff9921a586b303bea8d3568a0cb5
Adrián Navarro Gabino <adnaga94@gmail.com>
2019-11-03 18:25:10 +0100

    Challenge 117 - La fiesta aburrida

---
+++ Challenge0117.cpp
@@ -0,0 +1,20 @@
+// Adrián Navarro Gabino
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int cases;
+    cin >> cases;
+    string greeting;
+    
+    for(int i = 0; i < cases; i++)
+    {
+        cin >> greeting >> greeting;
+        cout << "Hola, " << greeting << "." << endl;
+    }
+    
+    return 0;
+}
