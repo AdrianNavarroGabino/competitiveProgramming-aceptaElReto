12c93970a19baf21daa3a37b3b34e40d85809460
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2019-12-15 09:46:22 +0100

    Challenge 265 - Suma descendente

---
+++ Challenge0265.cpp
@@ -0,0 +1,31 @@
+// Adrián Navarro Gabino
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string num;
+    int result;
+    
+    do
+    {
+        cin >> num;
+        
+        if(num != "0")
+        {
+            result = 0;
+            
+            while(num != "")
+            {
+                result += stoi(num);
+                num = num.substr(1);
+            }
+            
+            cout << result << endl;
+        }
+    } while(num != "0");
+    
+    return 0;
+}
