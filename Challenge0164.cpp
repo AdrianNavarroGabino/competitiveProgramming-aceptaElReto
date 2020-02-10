7772186c625dd6899f62cc8524dc08778dd469a1
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2020-02-10 11:58:15 +0100

    Challenge 164 - Área de un rectángulo

---
+++ Challenge0164.cpp
@@ -0,0 +1,17 @@
+// Adrián Navarro Gabino
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x1, y1, x2, y2;
+	
+	while(cin >> x1 >> y1 >> x2 >> y2 && x1 <= x2 && y1 <= y2)
+	{
+		cout << (x2 - x1) * (y2 - y1) << endl;
+	}
+	
+	return 0;
+}
